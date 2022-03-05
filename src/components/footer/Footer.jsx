9d3fa8519adb02089531169433821a5035586a81
AugustoTI <augustocesarti2021@gmail.com>
2022-03-05 19:19:59 -0300

    criando o Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import * as Styled from './footerStyled';
+import { ReactComponent as Insta } from '../../assets/insta.svg';
+import { ReactComponent as Linkedin } from '../../assets/linkedin.svg';
+import { ReactComponent as Twitter } from '../../assets/twitter.svg';
 
 export const Footer = () => {
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <Styled.Footer className="container">
+      <ul className="footer-links">
+        <li>
+          <a href="https://www.instagram.com/guzz_52/">
+            <Insta />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/augusto-césar-07114421b/">
+            <Linkedin />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/Guzz_TI">
+            <Twitter />
+          </a>
+        </li>
+      </ul>
+      <p>Copyright ©2020 All rights reserved </p>
+    </Styled.Footer>
   );
 };
---
+++ src/components/footer/footerStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  text-align: center;
+  padding: 4rem 0;
+
+  p {
+    color: var(--dark);
+    font-size: 1.4rem;
+    font-weight: 400;
+  }
+
+  .footer-links {
+    display: flex;
+    justify-content: center;
+    gap: 3.5rem;
+    margin-bottom: 2.5rem;
+  }
+`;
